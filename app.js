2a1d5ee5d472f2f5c1454a6a41cb1274ed04c907
Azizbek Ergashev <id.00011841@gmail.com>
2022-04-05 23:19:31 +0500

    hw

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-const express = require('express');
+const express = require("express");
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+app.get("/", (req, res) => {
+  res.send("hello world");
+});
+
+app.listen(proccess.env.PORT || 3000);
