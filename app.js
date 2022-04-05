5203b204b93369d4edbc849a27a9426574331fe7
Azizbek Ergashev <id.00011841@gmail.com>
2022-04-05 23:23:03 +0500

    configt

--- app.js
+++ app.js
@@ -1,9 +1,9 @@
-const express = require("express");
+import express from "express";
 const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.get("/", (req, res) => {
+app.get("/", (__, res) => {
   res.send("hello world");
 });
 
